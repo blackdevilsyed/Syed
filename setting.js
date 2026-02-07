7dbd103a75c2b426e2835b33df5a7a94be9ba30f
blackdevilsyed <syedabdulwahabbukhari8@gmail.com>
2026-02-07 20:36:54 +0500

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,15 @@
+const settings = {
+  packname: '𓆩 BLACK DEVIL 😈 𓆪',
+  author: '‎',
+  botName: "𓆩 SYED ABDUL WAHAB BUKHARI 𓆪",
+  botOwner: 'BLACK DEVIL', // Your name
+  ownerNumber: '923376539373', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/blackdevilsyed/Syed.git
+
+module.exports = settings;
